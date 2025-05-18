43ed2a700182234519d16d935be8a84c36ebc95b
Diwas poudel <diwaslearn@gmail.com>
2025-05-18 17:35:18 +0545

    Conditionally render price based on selected variant

--- app/(web)/products/[productName]/(components)/ProductInformation.tsx
+++ app/(web)/products/[productName]/(components)/ProductInformation.tsx
@@ -1,3 +1,245 @@
+// "use client";
+
+// import { useParams } from "next/navigation";
+// import { useState } from "react";
+// import { Flex, Heading, HStack, Stack, Text, VStack } from "@chakra-ui/react";
+
+// import { HeartIcon } from "@/assets/svg";
+// import { Button, ProductVariantTabs, QuantityInput } from "@/components";
+// import { useConfigQuery, useProductDetailByNameQuery } from "@/hooks/api";
+// import {
+//   useAuthCheck,
+//   useProductDetailCartMutation,
+//   useProductDetailWishlist,
+// } from "@/hooks/app";
+
+// import { ProductDescription } from "./ProductDescription";
+// import { ProductReviews } from "./ProductReviews";
+// import { VisibleSection } from "@/components/ui/visibleSection";
+
+// export const ProductInformation = () => {
+//   const params = useParams();
+
+//   const productName = params.productName as string;
+
+//   const { data: config } = useConfigQuery();
+
+//   const { data: productDetail } = useProductDetailByNameQuery(productName);
+
+//   const { handleAddToCart, isPending: isCartPending } =
+//     useProductDetailCartMutation();
+//   const { handleAddToWishlist, isPending: isWishlistPending } =
+//     useProductDetailWishlist();
+
+//   const minimumQuantity = productDetail?.custom_minimum_order_quantity || 1;
+//   const maximumQuantity = productDetail?.custom_maximum_order_quantity || 100;
+//   const incrementStep = productDetail?.custom_increment_on_quantity || 1;
+
+//   const [quantity, setQuantity] = useState(minimumQuantity);
+
+//   const handleQuantityChange = (newQuantity: number) => {
+//     if (newQuantity < minimumQuantity || newQuantity > maximumQuantity) return;
+
+//     setQuantity(newQuantity);
+//   };
+
+//   const price = productDetail?.prices?.[0]?.price_list_rate;
+
+//   const onAddToWishlist = () => {
+//     const payload = {
+//       item_code: productDetail?.item_code,
+//       quantity,
+//     };
+
+//     handleAddToWishlist(payload);
+//   };
+
+//   const onAddToCart = () => {
+//     const payload = {
+//       item_code: productDetail?.item_code,
+//       item_price: price || "",
+//       quantity,
+//     };
+//     handleAddToCart(payload);
+//   };
+
+//   const { checkAuth } = useAuthCheck();
+
+//   return (
+//     <Flex flex={1} flexDirection="column" width="100%">
+//       <VStack
+//         alignItems="stretch"
+//         gap={{ base: "20px", lg: "28px" }}
+//         paddingTop={{ base: "24px", md: "0" }}
+//         paddingBottom={{ base: "24px", lg: "40px" }}
+//         borderBottom={{ base: 0, md: "1px solid" }}
+//         borderColor={{ md: "system.neutral.separator.light" }}
+//       >
+//         {/* title */}
+//         <Stack gap={{ base: "12px", lg: "24px" }} width="100%">
+//           <Stack gap="12px">
+//             {/* <HStack justifyContent="space-between">
+//               <Text
+//                 color="system.text.light.light"
+//                 variant="subtitle2"
+//                 fontSize={{ base: "12px", lg: "14px" }}
+//               >
+//                 {productDetail?.item_group}
+//               </Text>
+//               <Text
+//                 color="system.text.light.light"
+//                 variant="subtitle2"
+//                 fontSize={{ base: "12px", lg: "14px" }}
+//               >
+//                 {productDetail?.item_code}
+//               </Text>
+//             </HStack> */}
+
+//             <Heading
+//               color="black"
+//               fontSize={{
+//                 base: "20px",
+//                 lg: "24px",
+//                 xl: "28px",
+//               }}
+//             >
+//               {productDetail?.item_name}
+//             </Heading>
+
+//             <Text
+//               color="system.text.light.light"
+//               variant="subtitle2"
+//               fontSize={{ base: "12px", lg: "14px" }}
+//             >
+//               {productDetail?.description}
+//             </Text>
+//           </Stack>
+//           <HStack gap="0">
+//             <VisibleSection visibility={config?.rate_visibility}>
+//               <Heading variant="heading6" fontWeight="400">
+//                 {config.currency} {price}
+//               </Heading>
+//             </VisibleSection>
+
+//             {/* <Text
+//               variant="subtitle1"
+//               color="danger.100"
+//               paddingLeft="12px"
+//               textDecoration="line-through"
+//             >
+//               रु{productDetail.originalPrice}
+//             </Text>
+
+//             <Text
+//               variant="subtitle2"
+//               borderRadius="8px"
+//               color="system.text.light.light"
+//               paddingLeft="4px"
+//             >
+//               -{productDetail.discount}
+//             </Text> */}
+//           </HStack>
+//         </Stack>
+
+//         {productDetail?.variants && (
+//           <Stack gap="12px">
+//             <Text variant="subtitle1" color="system.text.normal.light">
+//               Choose Variant
+//             </Text>
+
+//             <ProductVariantTabs variants={productDetail?.variants} />
+//           </Stack>
+//         )}
+
+//         {/* size */}
+//         {/* <Stack gap="12px" py={{ base: "24px", lg: "32px" }}>
+//           <HStack gap="0">
+//             <Text variant="subtitle1" color="system.text.normal.light">
+//               Choose Size
+//             </Text>
+//             <Text
+//               variant="subtitle2"
+//               fontSize="14px"
+//               color="system.text.light.light"
+//             >
+//               * All size are in EU
+//             </Text>
+//           </HStack>
+//           <HStack gap="12px">
+//             {productDetail?.sizes?.map((size, index) => (
+//               <Text
+//                 key={index}
+//                 variant="subtitle1"
+//                 color="system.text.primary.primary.light"
+//                 border="1px solid"
+//                 borderColor="system.neutral.separator.light"
+//                 padding="12px 16px"
+//                 width="fit-content"
+//               >
+//                 {size}
+//               </Text>
+//             ))}
+//           </HStack>
+//         </Stack> */}
+//         <VisibleSection visibility={config?.cart_visibility}>
+//           {/* quantity */}
+//           <Stack gap="12px">
+//             <Text variant="subtitle1" color="system.text.normal.light">
+//               Quantity
+//             </Text>
+//             <QuantityInput
+//               value={quantity}
+//               onChange={handleQuantityChange}
+//               minimum={minimumQuantity}
+//               maximum={maximumQuantity}
+//               incrementStep={incrementStep}
+//             />
+//           </Stack>
+//           {/* button */}
+//           <HStack gap="20px" width="100%">
+//             <VisibleSection visibility={config?.cart_visibility}>
+//               <Button
+//                 rounded={"3xl"}
+//                 bg={"#FF6996"}
+//                 type="submit"
+//                 flex={1}
+//                 onClick={checkAuth(onAddToCart)}
+//                 loading={isCartPending}
+//               >
+//                 Add to Bag
+//               </Button>
+//             </VisibleSection>
+//             <VisibleSection visibility={config?.wishlist_visibility}>
+//               <Button
+//                 borderRadius={"full"}
+//                 h={"10px"}
+//                 w={"10px"}
+//                 bg={"#FF6996"}
+//                 onClick={checkAuth(onAddToWishlist)}
+//                 loading={isWishlistPending}
+//               >
+//                 <HeartIcon
+//                   style={{
+//                     color: "white",
+//                   }}
+//                 />
+//               </Button>
+//             </VisibleSection>
+//           </HStack>
+//         </VisibleSection>
+//       </VStack>
+
+//       {/* description */}
+//       <ProductDescription />
+
+//       {/* reviews */}
+//       <VisibleSection visibility={config?.cart_visibility}>
+//         <ProductReviews item_code={productDetail?.item_code ?? ""} />
+//       </VisibleSection>
+//     </Flex>
+//   );
+// };
+
 "use client";
 
 import { useParams } from "next/navigation";
@@ -16,14 +258,14 @@ import {
 import { ProductDescription } from "./ProductDescription";
 import { ProductReviews } from "./ProductReviews";
 import { VisibleSection } from "@/components/ui/visibleSection";
+import { useVariantStore } from "@/store";
 
 export const ProductInformation = () => {
   const params = useParams();
-
   const productName = params.productName as string;
+  const { activeVariant } = useVariantStore();
 
   const { data: config } = useConfigQuery();
-
   const { data: productDetail } = useProductDetailByNameQuery(productName);
 
   const { handleAddToCart, isPending: isCartPending } =
@@ -43,11 +285,22 @@ export const ProductInformation = () => {
     setQuantity(newQuantity);
   };
 
-  const price = productDetail?.prices?.[0]?.price_list_rate;
+  // Get price based on whether product has variants or not
+  const getPrice = () => {
+    if (productDetail?.has_variants) {
+      const selectedVariant = productDetail?.variants?.find(
+        (variant) => variant.item_code === activeVariant
+      );
+      return selectedVariant?.prices?.[0]?.price_list_rate;
+    }
+    return productDetail?.prices?.[0]?.price_list_rate;
+  };
+
+  const price = getPrice();
 
   const onAddToWishlist = () => {
     const payload = {
-      item_code: productDetail?.item_code,
+      item_code: productDetail?.has_variants ? activeVariant : productDetail?.item_code,
       quantity,
     };
 
@@ -56,7 +309,7 @@ export const ProductInformation = () => {
 
   const onAddToCart = () => {
     const payload = {
-      item_code: productDetail?.item_code,
+      item_code: productDetail?.has_variants ? activeVariant : productDetail?.item_code,
       item_price: price || "",
       quantity,
     };
@@ -117,7 +370,7 @@ export const ProductInformation = () => {
           <HStack gap="0">
             <VisibleSection visibility={config?.rate_visibility}>
               <Heading variant="heading6" fontWeight="400">
-                {config.currency} {price}
+                {config?.currency} {price}
               </Heading>
             </VisibleSection>
 
@@ -141,7 +394,7 @@ export const ProductInformation = () => {
           </HStack>
         </Stack>
 
-        {productDetail?.variants && (
+        {productDetail?.has_variants && productDetail?.variants && (
           <Stack gap="12px">
             <Text variant="subtitle1" color="system.text.normal.light">
               Choose Variant
@@ -234,7 +487,9 @@ export const ProductInformation = () => {
 
       {/* reviews */}
       <VisibleSection visibility={config?.cart_visibility}>
-        <ProductReviews item_code={productDetail?.item_code ?? ""} />
+        <ProductReviews 
+          item_code={productDetail?.has_variants ? activeVariant : productDetail?.item_code ?? ""} 
+        />
       </VisibleSection>
     </Flex>
   );
