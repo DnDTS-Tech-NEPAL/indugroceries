3363df7f7fee177fc36056287eaea60ae580ec09
Diwas poudel <diwaslearn@gmail.com>
2025-06-03 11:23:35 +0545

    update swiper navigation

--- templates/default/pages/home/(components)/BrandCategory.tsx
+++ templates/default/pages/home/(components)/BrandCategory.tsx
@@ -1,7 +1,15 @@
 "use client";
 
 import Image from "next/image";
-import { Box, Button, Flex, Heading, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  IconButton,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { useConfigQuery, useHomePageQuery } from "@/hooks/api";
 import { useFeaturedBrandsImages } from "@/hooks/app";
 import { ROUTES } from "@/constants";
@@ -14,9 +22,13 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/autoplay";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { useRef } from "react";
+import SwiperCore from "swiper";
 
 export const BrandCategory = () => {
   const router = useRouter();
+  const swiperRef = useRef<SwiperCore | null>(null);
   const featureImages = useFeaturedBrandsImages();
   const { data: featuredBrandsData } = useHomePageQuery();
   const { data: config } = useConfigQuery();
@@ -40,48 +52,89 @@ export const BrandCategory = () => {
         px={{ base: 4, md: 6 }}
         py={{ base: 8, md: 8, lg: 12, "2xl": 16 }}
       >
-          <VStack align={{ base: "center"}} gap={1}>
-            <Heading
-              fontSize={{
-                base: "16px",
-                md: "20px",
-                lg: "28px",
-              }}
-              marginTop={{
-                base: "4px",
-                md: "12px",
-              }}
-              marginBottom={{
-                base: "0",
-                md: "8px",
-              }}
-            >
-              {featuredBrandTitle}
-            </Heading>
+        <VStack align={{ base: "center" }} gap={1}>
+          <Heading
+            fontSize={{
+              base: "16px",
+              md: "20px",
+              lg: "28px",
+            }}
+            marginTop={{
+              base: "4px",
+              md: "12px",
+            }}
+            marginBottom={{
+              base: "0",
+              md: "8px",
+            }}
+          >
+            {featuredBrandTitle}
+          </Heading>
 
-            <Text
-              fontSize={{ base: "xs", sm: "sm", "2xl": "md" }}
-              color="system.text.light.light"
-              textAlign={{ base: "center", sm: "left" }}
-            >
-              {featuredBrandSubtitle}
-            </Text>
+          <Text
+            fontSize={{ base: "xs", sm: "sm", "2xl": "md" }}
+            color="system.text.light.light"
+            textAlign={{ base: "center", sm: "left" }}
+          >
+            {featuredBrandSubtitle}
+          </Text>
 
-            <Text
-              fontSize={{ base: "sm", md: "md" }}
-              color="system.text.normal.light"
-              textAlign={{ base: "center", sm: "left" }}
-              maxW={{ base: "full", md: "3xl" }}
-            >
-              {featuredBrandDescription}
-            </Text>
-          </VStack>
+          <Text
+            fontSize={{ base: "sm", md: "md" }}
+            color="system.text.normal.light"
+            textAlign={{ base: "center", sm: "left" }}
+            maxW={{ base: "full", md: "3xl" }}
+          >
+            {featuredBrandDescription}
+          </Text>
+        </VStack>
 
         {/* Swiper Carousel with forced scrolling */}
-        <Box w="full" overflow="hidden" px={{ base: 0, md: 4 }}>
+        <Box
+          w="full"
+          overflow="hidden"
+          position={"relative"}
+          px={{ base: 0, md: 6 }}
+        >
+          {/* Custom Prev Arrow */}
+          <IconButton
+            aria-label="Prev"
+            onClick={() => swiperRef.current?.slidePrev()}
+            position="absolute"
+            left="0"
+            top="50%"
+            zIndex={20}
+            bg={"gray.100"}
+            height={30}
+            width={30}
+            color={"gray.100"}
+            transform="translateY(-50%)"
+            borderRadius="full"
+          >
+            <ChevronLeftIcon color="black" />
+          </IconButton>
+
+          {/* Custom Next Arrow */}
+          <IconButton
+            aria-label="Next"
+            onClick={() => swiperRef.current?.slideNext()}
+            position="absolute"
+            right="0"
+            top="50%"
+            height={30}
+            width={30}
+            transform="translateY(-50%)"
+            zIndex={20}
+            bg={"gray.100"}
+            borderRadius="full"
+          >
+            <ChevronRightIcon color="black" />
+          </IconButton>
+
           <Swiper
             modules={[Autoplay]}
             spaceBetween={10}
+            onSwiper={(swiper) => (swiperRef.current = swiper)}
             slidesPerView={baseSlides}
             autoplay={{
               delay: 2500,
@@ -111,7 +164,7 @@ export const BrandCategory = () => {
                 <Box
                   transition="all 0.3s ease"
                   p={{ base: 2, md: 3 }}
-                  borderRadius="md"   
+                  borderRadius="md"
                   cursor="pointer"
                   onClick={() =>
                     router.push(`${ROUTES.APP.PRODUCTS}?brands=${item.name}`)
@@ -124,7 +177,6 @@ export const BrandCategory = () => {
                       base: "100px",
                       sm: "120px",
                       md: "140px",
-                 
                     }}
                     borderRadius="md"
                     overflow="hidden"
@@ -136,7 +188,6 @@ export const BrandCategory = () => {
                       style={{
                         objectFit: "contain",
                         objectPosition: "center",
-                      
                       }}
                       sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                     />
