adda07bd927e9f57f5dba37805def5ffb668d185
alishabirat <alishabirat@gmail.com>
2025-06-25 17:28:10 +0545

    Update page.tsx
    implement base layout for checkout page

--- app/(user)/checkout/page.tsx
+++ app/(user)/checkout/page.tsx
@@ -1,9 +1,20 @@
-import ShippingInformation from "./(component)/ShippingInformation";
+import { PageTitle } from "@/components";
+import { BREADCRUMB_CONFIG } from "@/config";
+import { ROUTES } from "@/constants";
+import CheckoutSection from "./(component)/Checkout";
 
-const Checkout = () => {
+
+
+const Checkout = async () => {
   return (
     <>
-      <ShippingInformation />
+      <PageTitle
+        backLabel="Back to homepage"
+        backLink={ROUTES.APP.HOMEPAGE}
+        title="Checkout"
+        breadcrumb={BREADCRUMB_CONFIG.CHECKOUT}
+      />
+     <CheckoutSection/>
     </>
   );
 };
