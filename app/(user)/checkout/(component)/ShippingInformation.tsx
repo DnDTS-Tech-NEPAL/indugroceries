0877ae61056c961df05b86746baa0a1a25f7a6f6
alishabirat <alishabirat@gmail.com>
2025-06-25 14:24:23 +0545

    Create toggleable shipping information section on checkout page based on Figma design

--- app/(user)/checkout/(component)/ShippingInformation.tsx
+++ app/(user)/checkout/(component)/ShippingInformation.tsx
@@ -1,65 +1,142 @@
-import { Box, Heading, Stack, Input } from "@chakra-ui/react";
+"use client";
+
+import { Box, Stack, Input, Text, VStack } from "@chakra-ui/react";
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Portal, Select, createListCollection } from "@chakra-ui/react";
+import { AccordionItem, AccordionItemContent, AccordionItemTrigger, AccordionRoot } from "@/components";
+
+const countriesCollection = createListCollection({
+  items: [
+    { label: "United States", value: "us" },
+    { label: "Canada", value: "ca" },
+    { label: "United Kingdom", value: "uk" },
+  ],
+});
+
+const provincesCollection = createListCollection({
+  items: [
+    { label: "California", value: "ca" },
+    { label: "New York", value: "ny" },
+    { label: "Texas", value: "tx" },
+
+  ],
+});
 
 const ShippingInformation = () => {
+  const title = "Shipping Information";
+
   return (
-    <Box p={6} borderWidth="1px" borderRadius="lg" boxShadow="md">
-      <Heading as="h2" size="lg" mb={6}>
-        Shipping Information
-      </Heading>
+    <VStack alignItems="stretch">
+      <AccordionRoot
+        collapsible
+        as={VStack}
+        alignItems="stretch"
+        defaultValue={[title]}
+      >
+        <AccordionItem value={title} border="none">
+          <AccordionItemTrigger hasAccordionIcon isOpen>
+            <Text fontSize="xl" fontWeight={500}>
+              {title}
+            </Text>
+          </AccordionItemTrigger>
+          <AccordionItemContent>
+            <VStack align="start" gap={4} spaceY={4} mt={4}>
+              {/* Name Section */}
+              <Stack direction={{ base: "column", md: "row" }}  gap={6} width="full">
+                <FormControl isRequired flex={1}>
+                  <FormLabel>First Name</FormLabel>
+                  <Input placeholder="First Name" />
+                </FormControl>
+                <FormControl isRequired flex={1} >
+                  <FormLabel>Last Name</FormLabel>
+                  <Input placeholder="Last Name" />
+                </FormControl>
+              </Stack>
 
-      <Stack gap={4}>
-        {/* Name Section */}
-        <Stack direction={{ base: "column", md: "row" }} gap={4}>
-          <FormControl isRequired>
-            <FormLabel fontWeight="bold">First Name</FormLabel>
-            <Input placeholder="First Name" />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel fontWeight="bold">Last Name</FormLabel>
-            <Input placeholder="Last Name" />
-          </FormControl>
-        </Stack>
+              {/* Contact Section */}
+              <Stack direction={{ base: "column", md: "row" }} gap={4} width="full">
+                <FormControl isRequired flex={1}>
+                  <FormLabel>Email Address</FormLabel>
+                  <Input type="email" placeholder="Email Address" />
+                </FormControl>
+                <FormControl isRequired flex={1}>
+                  <FormLabel>Phone Number</FormLabel>
+                  <Input type="tel" placeholder="Phone Number" />
+                </FormControl>
+              </Stack>
 
-        {/* Contact Section */}
-        <Stack direction={{ base: "column", md: "row" }} gap={4}>
-          <FormControl isRequired>
-            <FormLabel fontWeight="bold">Email Address</FormLabel>
-            <Input type="email" placeholder="Email Address" />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel fontWeight="bold">Phone Number</FormLabel>
-            <Input type="tel" placeholder="Phone Number" />
-          </FormControl>
-        </Stack>
+              {/* Address Section */}
+              <Stack direction={{ base: "column", md: "row" }} gap={4} width="full">
+              <FormControl isRequired width="full" flex={1}>
+                <FormLabel>Country / Region</FormLabel>
+                <Select.Root collection={countriesCollection} size="sm">
+                  <Select.HiddenSelect />
+                  <Select.Control>
+                    <Select.Trigger>
+                      <Select.ValueText placeholder="Select country" />
+                    </Select.Trigger>
+                    <Select.IndicatorGroup>
+                      <Select.Indicator />
+                    </Select.IndicatorGroup>
+                  </Select.Control>
+                  <Portal>
+                    <Select.Positioner>
+                      <Select.Content>
+                        {countriesCollection.items.map((country) => (
+                          <Select.Item item={country} key={country.value}>
+                            {country.label}
+                            <Select.ItemIndicator />
+                          </Select.Item>
+                        ))}
+                      </Select.Content>
+                    </Select.Positioner>
+                  </Portal>
+                </Select.Root>
+              </FormControl>
+              </Stack>
 
-        {/* Address Section */}
-        <FormControl isRequired>
-          <FormLabel fontWeight="bold">Country / Region</FormLabel>
-          <select>
-            <option value="us">United States</option>
-            <option value="ca">Canada</option>
-            <option value="uk">United Kingdom</option>
-            {/* Add more countries as needed */}
-          </select>
-        </FormControl>
-
-        <Stack direction={{ base: "column", md: "row" }} gap={4}>
-          <FormControl isRequired>
-            <FormLabel fontWeight="bold">Province</FormLabel>
-            <Input placeholder="Province" />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel fontWeight="bold">City</FormLabel>
-            <Input placeholder="City" />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel fontWeight="bold">Zip Code</FormLabel>
-            <Input placeholder="Zip Code" />
-          </FormControl>
-        </Stack>
-      </Stack>
-    </Box>
+              <Stack direction={{ base: "column", md: "row" }} gap={4} width="full">
+                <FormControl isRequired flex={1}>
+                  <FormLabel>Province/State</FormLabel>
+                  <Select.Root collection={provincesCollection} size="sm">
+                    <Select.HiddenSelect />
+                    <Select.Control>
+                      <Select.Trigger>
+                        <Select.ValueText placeholder="Select province" height={"45px"} display={"flex"} alignItems={"center"} />
+                      </Select.Trigger>
+                      <Select.IndicatorGroup>
+                        <Select.Indicator />
+                      </Select.IndicatorGroup>
+                    </Select.Control>
+                    <Portal>
+                      <Select.Positioner>
+                        <Select.Content>
+                          {provincesCollection.items.map((province) => (
+                            <Select.Item item={province} key={province.value}>
+                              {province.label}
+                              <Select.ItemIndicator />
+                            </Select.Item>
+                          ))}
+                        </Select.Content>
+                      </Select.Positioner>
+                    </Portal>
+                  </Select.Root>
+                </FormControl>
+                
+                <FormControl isRequired flex={1}>
+                  <FormLabel>City</FormLabel>
+                  <Input placeholder="City" />
+                </FormControl>
+                <FormControl isRequired>
+                  <FormLabel>Zip Code</FormLabel>
+                  <Input placeholder="Zip Code" />
+                </FormControl>
+              </Stack>
+            </VStack>
+          </AccordionItemContent>
+        </AccordionItem>
+      </AccordionRoot>
+    </VStack>
   );
 };
 
