d4717ae8a6faccf554a76dcdd13fc87d49c6f77e
Diwas poudel <diwaslearn@gmail.com>
2025-06-26 12:28:40 +0545

    added item_group ribbon in product image in product single page

--- types/app/products/productImageDisplay.ts
+++ types/app/products/productImageDisplay.ts
@@ -1,4 +1,6 @@
+import { IndividualProductAPIType } from "@/types/api";
 export type ProductImageDisplayProps = {
   selectedImage: string;
   productName: string;
+  productDetail: IndividualProductAPIType;
 };
