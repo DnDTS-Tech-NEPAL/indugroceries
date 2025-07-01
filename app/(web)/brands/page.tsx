5926a1a4d933c011886c61d64ca854434159e4c7
alishabirat <alishabirat@gmail.com>
2025-07-01 14:39:18 +0545

    Create Hero Section and import in main page

--- app/(web)/brands/page.tsx
+++ app/(web)/brands/page.tsx
@@ -4,6 +4,7 @@ import { PageTitle } from "@/components";
 import { BREADCRUMB_CONFIG } from "@/config";
 import { ROUTES } from "@/constants";
 import AllBrands from "./(components)/AllBrands";
+import HeroSection from "./(components)/HeroSection";
 
 export const metadata: Metadata = {
   title: "Brands",
@@ -21,6 +22,7 @@ const Brands = () => {
         title="All Brands"
         breadcrumb={BREADCRUMB_CONFIG.BRANDS}
       />
+      <HeroSection/>
       <AllBrands />
     </>
   );
